7f6f2f9ccec270d4f647d6790c78744005f53d6a
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-02-10 22:52:37 -0300

    Add files via upload

---
+++ exercicio 7.py
@@ -0,0 +1,5 @@
+# Exercício 7 - Adicione mais um elemento ao dicionário criado no exercício anterior e imprima na tela
+
+dicionario = {'valor1':5, 'valor2':10, 'valor3':25}
+dicionario['valor4'] = 30
+print(dicionario)
