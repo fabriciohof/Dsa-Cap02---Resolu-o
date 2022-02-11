77fd992e5e7f2706a1a354fb91f9cba06b0a2c13
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-02-10 22:52:58 -0300

    Add files via upload

---
+++ exercicio 9.py
@@ -0,0 +1,7 @@
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dicionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+
+elementos = ('esfregão',('banana','feijão'), {'chave1':'sete','chave2':'nove'},float(3.2))
+print(elementos)
