087bd8a7e004c95393af5efbf94cb0984242e4f2
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-02-10 22:52:48 -0300

    Add files via upload

---
+++ exercicio 8.py
@@ -0,0 +1,6 @@
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores.
+# Um dos valores deve ser uma lista de 2 elementos numéricos. 
+# Imprima o dicionário na tela
+
+dict = {'chave1':'coelho', 'chave2':'guaxinim','chave3':[3,21]}
+print(dict)
